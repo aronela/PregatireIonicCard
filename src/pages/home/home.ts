968888a0b208c6e76c742d1497520c61455c048e
aronela <aronela.lucaci@emanuel.ro>
2019-02-07 01:02:48 +0200

    Primul commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public countLike: number = 0;
+  public comments: { name: string, comment: string } [] = [{
+    name: 'Vanda',
+    comment: 'very nice!'
+  }];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertController: AlertController) {
 
   }
+
+  clickLike() {
+    this.countLike++;
+  }
+
+  clickComment() {
+    let addReminderAlert = this.alertController.create({
+      title: "Leave a comment",
+      inputs: [
+        {
+          placeholder: 'Your name',
+          name: 'nameInput',
+        },
+        {
+          placeholder: 'Your comment',
+          name: 'commentInput',
+        },
+      ],
+      buttons: [
+        {
+        text: 'Cancel',
+        },
+        {
+          text: 'Post',
+          handler: (inputData) => {
+            if(inputData.nameInput && inputData.commentInput) {
+              this.comments.unshift(
+              {
+                name: inputData.nameInput,
+                comment: inputData.commentInput,
+              }
+
+              )
+            }
+          }
+        },
+      ]
+    });
+    addReminderAlert.present();
+  }
+
+
 
 }
